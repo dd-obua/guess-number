ca4f9f46c80726eba04de703b7f27228b08a3e1e
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-14 17:12:32 +0300

    Refactor and comment js file (while keeping its functionality)

--- js/script.js
+++ js/script.js
@@ -2,61 +2,74 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+// Hold document objects in variables
+const checkBtn = document.querySelector('.check');
+const outputMsg = document.querySelector('.msg');
+const result = document.querySelector('.result');
+const highestScore = document.querySelector('.highest-score');
+const body = document.querySelector('body');
+const guessInput = document.querySelector('.guess');
+const scoreOuput = document.querySelector('.score');
+const reset = document.querySelector('.reset');
+
+// Set initial score values
 let score = 20;
 let maxScore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+checkBtn.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
 
+  // No input
   if (!guess) {
-    document.querySelector('.msg').textContent = 'No number!';
-  } else if (guess < 1 || guess > 20) {
-    document.querySelector('.msg').textContent = 'Out of range (1 to 20)';
-  } else if (guess === secretNumber) {
-    document.querySelector('.result').textContent = guess;
-    document.querySelector('.msg').textContent = 'Correct number!';
-    if (score > maxScore) maxScore = score;
-    document.querySelector('.highest-score').textContent = maxScore;
+    outputMsg.textContent = 'No number!';
+  }
 
-    document.querySelector('body').style.background = '#60b347';
-    document.querySelector('.guess').style.background = '#60b347';
-    document.querySelector('.result').style.fontSize = '80px';
-    document.querySelector('.result').style.padding = '24px 48px';
-    document.querySelector('.result').style.borderRadius = '100px';
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.msg').textContent = 'Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.msg').textContent = 'You lost the game';
-      score = 0;
-      document.querySelector('.score').textContent = score;
+  // Correct guess
+  else if (guess === secretNumber) {
+    result.textContent = guess;
+    outputMsg.textContent = 'Correct number!';
+    if (score > maxScore) maxScore = score;
+    highestScore.textContent = maxScore;
+
+    body.style.background = '#60b347';
+    guessInput.style.background = '#60b347';
+    result.style.fontSize = '80px';
+    result.style.padding = '24px 48px';
+    result.style.borderRadius = '100px';
+  }
+
+  // Wrong guess
+  else if (guess !== secretNumber) {
+    // Out of range
+    if (guess < 1 || guess > 20) {
+      outputMsg.textContent = 'Out of range (1 to 20)';
     }
-  } else {
-    if (score > 1) {
-      document.querySelector('.msg').textContent = 'Too high!';
+    // Within range
+    else if (score > 1) {
+      outputMsg.textContent = guess < secretNumber ? 'Too low!' : 'Too high!';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreOuput.textContent = score;
     } else {
-      document.querySelector('.msg').textContent = 'You lost the game';
+      outputMsg.textContent = 'You lost the game';
       score = 0;
-      document.querySelector('.score').textContent = score;
+      scoreOuput.textContent = score;
     }
   }
 });
 
-document.querySelector('.reset').addEventListener('click', function () {
-  document.querySelector('body').style.background = '#333';
-  document.querySelector('.result').style.fontSize = '60px';
-  document.querySelector('.result').style.padding = '8px 36px';
-  document.querySelector('.result').style.borderRadius = '0';
-  document.querySelector('.guess').style.background = '#333';
+reset.addEventListener('click', function () {
+  body.style.background = '#333';
+  result.style.fontSize = '60px';
+  result.style.padding = '8px 36px';
+  result.style.borderRadius = '0';
+  guessInput.style.background = '#333';
 
-  document.querySelector('.result').textContent = '?';
-  document.querySelector('.guess').value = '';
-  document.querySelector('.msg').textContent = 'Start guessing ...';
+  result.textContent = '?';
+  guessInput.value = '';
+  outputMsg.textContent = 'Start guessing ...';
   score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.highest-score').textContent = maxScore;
+  scoreOuput.textContent = score;
+  highestScore.textContent = maxScore;
+
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
