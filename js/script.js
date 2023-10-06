f8388f56a0d13fc38feb091d60107470ddeea3b3
Obua Denis Daniel <dd.obua@gmail.com>
2023-10-06 11:31:15 +0300

    Refactor script.js file

--- js/script.js
+++ js/script.js
@@ -1,80 +1,57 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 const select = selector => document.querySelector(selector);
 
-const checkBtn = select('.check');
-const outputMsg = select('.msg');
-const result = select('.result');
-const highestScore = select('.highest-score');
-const body = select('body');
-const guessInput = select('.guess');
-const scoreOuput = select('.score');
-const reset = select('.reset');
+const scoreElem = select('.score');
+const secretNumberElem = select('.number');
+const checkElem = select('.check');
+const messageElem = select('.message');
+const bodyElem = select('body');
+const highScoreElem = select('.highscore');
+const againElem = select('.again');
+const guessElem = select('.guess');
 
-// Refactor using functions
-const displayMsg = function (msg) {
-  outputMsg.textContent = msg;
-};
-
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let maxScore = 0;
+let highScore = 0;
 
-checkBtn.addEventListener('click', function () {
-  const guess = Number(guessInput.value);
+const updateScore = score => (scoreElem.textContent = score);
+const updateSecretNumber = num => (secretNumberElem.textContent = num);
+const updateMessage = msg => (messageElem.textContent = msg);
 
-  // No input
-  if (!guess) {
-    displayMsg('No number!');
-  }
+updateScore(score);
+updateSecretNumber('?');
 
-  // Correct guess
-  else if (guess === secretNumber) {
-    result.textContent = guess;
-    displayMsg('Correct number!');
-    if (score > maxScore) maxScore = score;
-    highestScore.textContent = maxScore;
+checkElem.addEventListener('click', () => {
+  const guess = Number(select('.guess').value);
 
-    body.style.background = '#60b347';
-    guessInput.style.background = '#60b347';
-    result.style.fontSize = '80px';
-    result.style.padding = '24px 48px';
-    result.style.borderRadius = '100px';
-  }
-
-  // Wrong guess
-  else if (guess !== secretNumber) {
-    // Out of range
-    if (guess < 1 || guess > 20) {
-      displayMsg('Out of range (1 to 20)');
-    }
-    // Within range
-    else if (score > 1) {
-      displayMsg(guess < secretNumber ? 'Too low!' : 'Too high!');
-      score--;
-      scoreOuput.textContent = score;
+  if (guess) {
+    if (score <= 1) {
+      updateMessage('You lost the game!');
+      updateScore(0);
     } else {
-      displayMsg('You lost the game');
-      score = 0;
-      scoreOuput.textContent = score;
+      if (guess === secretNumber) {
+        updateMessage('Correct number!');
+        updateSecretNumber(secretNumber);
+        bodyElem.style.background = '#60b347';
+        secretNumberElem.style.width = '30rem';
+        if (score > highScore) highScore = score;
+        highScoreElem.textContent = highScore;
+      } else {
+        updateMessage(`Too ${guess > secretNumber ? 'high' : 'low'}!`);
+        score--;
+        updateScore(score);
+      }
     }
-  }
+  } else updateMessage('No number');
 });
 
-reset.addEventListener('click', function () {
-  body.style.background = '#333';
-  result.style.fontSize = '60px';
-  result.style.padding = '8px 36px';
-  result.style.borderRadius = '0';
-  guessInput.style.background = '#333';
-
-  result.textContent = '?';
-  guessInput.value = '';
-  outputMsg.textContent = 'Start guessing ...';
+againElem.addEventListener('click', () => {
   score = 20;
-  scoreOuput.textContent = score;
-  highestScore.textContent = maxScore;
-
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  updateSecretNumber('?');
+  updateMessage('Start guessing...');
+  guessElem.value = '';
+  updateScore(score);
+  bodyElem.style.background = '#222';
+  secretNumberElem.style.width = '15rem';
 });
